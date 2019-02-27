ca34154f1c9629f98041d1b49e4fd229d27b9e6e
Erick Guerrero <erickg@valles.udg.mx>
2019-02-27 17:45:50 -0600

    Se añadió Picasso

--- app/src/main/java/com/example/librerias/MainActivity.kt
+++ app/src/main/java/com/example/librerias/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.squareup.picasso.Picasso
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+
 
         val palabras = listOf("Esta","es","mi","lista","de","palabras")//size = 6
 
@@ -31,8 +34,11 @@ class MainActivity : AppCompatActivity() {
             }
 
 
-            //Snackbar.make(view, "Se ha cambiado la palabra", Snackbar.LENGTH_LONG)
-              //  .setAction("Action", null).show()
+            Snackbar.make(view, "Se ha cambiado la palabra", Snackbar.LENGTH_LONG)
+                .setAction("Ver imagen", {
+                    Picasso.get().load("https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/5c3871215bafe83b078adbe3/perro.jpg")
+                        .into(miImageView)
+                }).show()
         }
     }
 
